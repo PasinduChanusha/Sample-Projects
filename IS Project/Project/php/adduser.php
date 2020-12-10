50aff5e30995b0fe76b8e2f686744a022d098d5f
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2020-12-10 22:33:53 +0530

    Add files via upload

---
+++ IS Project/Project/php/adduser.php
@@ -0,0 +1,32 @@
+<?php
+require_once('db.php');
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$fname= $_POST["fname"];
+$lname=$_POST["lname"];
+$email=$_POST["email"];
+$type= $_POST["type"];
+$pw=md5($_POST["password"]);
+
+
+
+$sql = "INSERT INTO users(`fname`, `lname`, `email`, `type`, `password`) VALUES ('$fname','$lname','$email','$type','$pw');";
+
+if ($conn->query($sql) === TRUE) {
+    echo '<script language="javascript">';
+	echo 'alert("User added");';
+	echo 'window.location.href="../manageusers.php";';
+	echo '</script>';
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
+
